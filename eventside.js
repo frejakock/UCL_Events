9979b11752e29f6579faa12f43f984a06a744f8a
Alberte-Adelhejd <alberteadelhejd00@icloud.com>
2022-11-08 10:19:54 +0100

    Update eventside.js

--- eventside.js
+++ eventside.js
@@ -13,4 +13,27 @@ function myFunction() {
     btnText.innerHTML = "Læs mindre";
     moreText.style.display = "inline";
   }
+
 }
+
+var modal = document.getElementById("Modal");
+
+var btn = document.getElementById("modalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+console.log("windows clicked")
+if (event.target == modal) {
+modal.style.display = "none";
+ }
+ }
